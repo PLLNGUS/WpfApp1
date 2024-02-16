a913a3dedf328cd4e92f2bf968ce248276b9544a
elnar <elnar.minigaliew@gmail.com>
2024-02-16 13:30:24 +0300

    Third commit

--- WpfApp1/Window1.xaml.cs
+++ WpfApp1/Window1.xaml.cs
@@ -23,5 +23,15 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_PreviewDragEnter(object sender, DragEventArgs e)
+        {
+
+        }
     }
 }
